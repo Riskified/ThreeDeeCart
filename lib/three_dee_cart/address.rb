2cedc14b87e75045815f73a00ed2d8e11ee304d1
Miki Bergin <shimi.b@gmail.com>
2013-07-06 21:51:36 +0300

    address fields correct syntax

--- lib/three_dee_cart/address.rb
+++ lib/three_dee_cart/address.rb
@@ -1,13 +1,13 @@
 module ThreeDeeCart
   class Address < ThreeDeeCart::Root
-    attr_accessor :firstName
-    attr_accessor :lastName
+    attr_accessor :first_name
+    attr_accessor :last_name
     attr_accessor :address
     attr_accessor :address2
     attr_accessor :city
-    attr_accessor :stateCode
-    attr_accessor :zipCode
-    attr_accessor :countryCode
+    attr_accessor :state_code
+    attr_accessor :zip_code
+    attr_accessor :country_code
     attr_accessor :company
     attr_accessor :phone
   end
